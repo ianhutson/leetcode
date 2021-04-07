b5697e1a7302a362baa181c1614cfeeae2f81862
ianhutson <ih00209@georgiasouthern.edu>
2021-04-07 13:22:38 -0400

    done

---
+++ how-many-smaller-nums.js
@@ -0,0 +1,10 @@
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+var smallerNumbersThanCurrent = function(nums) {
+    let arr = []
+    for (let i = 0; nums.length > i; i++){
+        if 
+    }
+};

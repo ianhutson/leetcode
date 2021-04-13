7a02becb0df68881a1ead0f7039b10fd848149d2
ianhutson <ih00209@georgiasouthern.edu>
2021-04-13 13:18:13 -0400

    done

--- skilled-mock-interview.js
+++ skilled-mock-interview.js
@@ -173,6 +173,13 @@ var findMostRepeated = function(x){
 }
 
 
-
-
-youtube.com/AlphaEchoes
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+	     let numToFind = target - nums[i];
+		 for(let j = i+1; j < nums.length; j++) {
+            if(numToFind ===nums[j])
+                return [i,j];
+        }
+    }
+    return null;
+}; 

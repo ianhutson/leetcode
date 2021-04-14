f18cf95b5709c178f5e059ee00905e80cbe342ba
ianhutson <ih00209@georgiasouthern.edu>
2021-04-14 10:20:58 -0400

    refactored 2 sum

--- two-sum.js
+++ two-sum.js
@@ -37,3 +37,20 @@
         comp[target-nums[i]] = i
     }
 };
+
+
+
+
+var twoSum = function(nums, target){
+    const prevValues = {}
+    for (let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i]
+        const neededValue = target - currentNumber
+        const index2 = prevValues[neededValue]
+        if (index2 != null){
+            return [index2, i]
+        } else {
+            prevValues[currentNumber] = i
+        }
+    }
+}

aef8c10e4f3404040756f9fa545d22b4f6b21294
ianhutson <57458226+ianhutson@users.noreply.github.com>
2022-04-13 14:43:28 -0400

    done

--- remove-element.js
+++ remove-element.js
@@ -1,13 +1,10 @@
 var removeElement = function(nums, val) {
-    let numbersRemoved = nums;
-    while (numbersRemoved.includes(val)){
-        numbersRemoved.splice(nums.indexOf(val),1)
-    };
-    return numbersRemoved.length
-   };
+    for(let i = nums.length; i >= 0; i--){
+        if (nums[i] == val){
+            nums.splice(i,1)
+        }
+    }
+    return nums.length
+};
 
-
-removeElement([3,2,2,3],3)
-// removeElement([0,1,2,2,3,0,4,2],2)
-
-// dentistrybyjrw@gmail.com
+removeElement([0,1,2,2,3,0,4,2],2)

31585658d0eda342fc0dcc3ebf589088ce5d4802
ianhutson <57458226+ianhutson@users.noreply.github.com>
2022-04-15 09:05:41 -0400

    done

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,38 +1,30 @@
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// // easy way
 
-// Example 1:
+// var searchInsert = function(nums, target) {
+//     if(nums.includes(target)) return nums.indexOf(target)
+//     else if(nums[nums.length-1] < target) return nums.length
+//     else if (nums[0] > target) return 0
+//         for(let i=0; nums.length > i; i++){
+//             if(nums[i] < target && nums[i+1] > target){
+//                 return i+1
+//             }
+//         }         
+// }
 
-// Input: nums = [1,3,5,6], target = 5
-// Output: 2
-// Example 2:
-
-// Input: nums = [1,3,5,6], target = 2
-// Output: 1
-// Example 3:
-
-// Input: nums = [1,3,5,6], target = 7
-// Output: 4
-// Example 4:
-
-// Input: nums = [1,3,5,6], target = 0
-// Output: 0
-// Example 5:
-
-// Input: nums = [1], target = 0
-// Output: 0
- 
-
-// Constraints:
-
-// 1 <= nums.length <= 104
-// -104 <= nums[i] <= 104
-// nums contains distinct values sorted in ascending order.
-// -104 <= target <= 104
-
+// faster way
 var searchInsert = function(nums, target) {
-    let copy = nums.slice()
-    copy.push(target)
-    let sorted = copy.sort(function(a,b) { return a - b; })
-    if (nums.includes(target)) return nums.indexOf(target)
-    else return sorted.indexOf(target)
-};
+    if(nums.includes(target)) return nums.indexOf(target)
+    else if(nums[nums.length-1] < target) return nums.length
+    else if (nums[0] > target) return 0
+    else {
+        const closest = nums.reduce((a, b) => {
+            return Math.abs(b - target) < Math.abs(a - target) ? b : a;
+        });
+        if(closest > target){
+            return nums.indexOf(closest)
+        } else return nums.indexOf(closest) + 1
+    }
+}
+
+
+searchInsert([1,3,5,6], 7)
